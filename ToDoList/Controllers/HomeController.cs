e11416df3b21c57b1ac38e129314cb059b9475c6
Mel Yasuda and Victoria Oh <mel@>
2018-09-24 09:22:14 -0700

    Update project to follow along in C# week 4 todo lesson.

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,14 +1,14 @@
-// using Microsoft.AspNetCore.Mvc;
-//
-// namespace ToDoList.Controllers
-// {
-//     public class HomeController : Controller
-//     {
-//
-//         [HttpGet("/")]
-//         public ActionResult Index()
-//         {
-//             return View();
-//         }
-//     }
-// }
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -132,6 +132,33 @@ namespace ToDoList.Models
             return newItem;
         }
 
+        public void Edit(string newDescription)
+       {
+           MySqlConnection conn = DB.Connection();
+           conn.Open();
+           var cmd = conn.CreateCommand() as MySqlCommand;
+           cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+
+           MySqlParameter searchId = new MySqlParameter();
+           searchId.ParameterName = "@searchId";
+           searchId.Value = _id;
+           cmd.Parameters.Add(searchId);
+
+           MySqlParameter description = new MySqlParameter();
+           description.ParameterName = "@newDescription";
+           description.Value = newDescription;
+           cmd.Parameters.Add(description);
+
+           cmd.ExecuteNonQuery();
+           _description = newDescription;
+
+           conn.Close();
+           if (conn != null)
+           {
+               conn.Dispose();
+           }
+       }
+
         public static void DeleteAll()
         {
             MySqlConnection conn = DB.Connection();
